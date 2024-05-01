5466e8dd3cdd41e7f31309e60542fe69f4ad5505
Fernando <fer3443@gmail.com>
2024-05-01 18:16:01 -0300

    boton de configuracion agregado

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import users from "./user.js";
-import {renderChat, handleButtonMenu2} from "../src/scripts/funciones.js";
+import { renderChat, handleButtonMenu2 } from "../src/scripts/funciones.js";
 (() => {
   const cardContainer = document.querySelector("#content-chat-card");
   const btnMenu = document.querySelector("#buton-menu");
   const menuOptions = document.querySelector("#menu-options");
-  renderChat(users,cardContainer);
+  renderChat(users, cardContainer);
 
   //manejo del boton menu
   const handleButtonMenu = () => {
@@ -13,30 +13,43 @@ import {renderChat, handleButtonMenu2} from "../src/scripts/funciones.js";
   };
   btnMenu.addEventListener("click", () => handleButtonMenu());
 
-  //manejo de menus-options2 
+  //manejo de menus-options2
   const allBtnChev = document.querySelectorAll(".chevron");
   handleButtonMenu2(allBtnChev);
 
- //manejo de menus-options para cerrado automatico
- const menuOptions2 = document.querySelectorAll(".menu-options2")
+  //manejo de menus-options para cerrado automatico
+  const menuOptions2 = document.querySelectorAll(".menu-options2");
   document.addEventListener("click", (event) => {
     let clicFueraMenu = true;
-    menuOptions2.forEach(menu => {
-        if (menu.contains(event.target)) {
-            clicFueraMenu = false;
-        }
+    menuOptions2.forEach((menu) => {
+      if (menu.contains(event.target)) {
+        clicFueraMenu = false;
+      }
     });
-    allBtnChev.forEach(btn => {
-        if (btn.contains(event.target)) {
-            clicFueraMenu = false;
-        }
+    allBtnChev.forEach((btn) => {
+      if (btn.contains(event.target)) {
+        clicFueraMenu = false;
+      }
     });
     if (clicFueraMenu) {
-        menuOptions2.forEach(menu => {
-            if (menu.classList.contains("show-menu2")) {
-                menu.classList.remove("show-menu2");
-            }
-        });
+      menuOptions2.forEach((menu) => {
+        if (menu.classList.contains("show-menu2")) {
+          menu.classList.remove("show-menu2");
+        }
+      });
+    }
+  });
+  const btnChats = document.querySelector("#btn-chats");
+  const btnConfig = document.querySelector("#btn-config");
+  const menuConfig = document.querySelector("#config-container");
+
+  const handleButtonConfig = () => {
+    menuConfig.classList.toggle("show-config");
+  };
+  btnConfig.addEventListener("click", () => handleButtonConfig());
+  btnChats.addEventListener("click", () => {
+    if (menuConfig.classList.contains("show-config")) {
+      menuConfig.classList.remove("show-config");
     }
   });
 })();
